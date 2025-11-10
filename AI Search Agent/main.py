491dc9bd3f83d37091059618a00bed3b789a485d
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-11-10 09:33:22 +0530

    Import langgraph libs
    Import langgraph libs

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -2,4 +2,4 @@ from dotenv import load_dotenv
 from typing import Annotated
 from langgraph.graph import StateGraph, START, END
 load_dotenv()
-from langgraph.graph.message import add
+from langgraph.graph.message import add_messages
