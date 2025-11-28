2f2eb85eeba7d5d6e225260803c722a32fef66e5
IT21314742 <it21314742@my.sliit.lk>
2025-11-28 19:00:11 +0530

    define Bing Search Query
    define Bing Search Query

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -38,7 +38,7 @@ def google_search(state: State):
     bing_results = serp_search(user_question, engine="bing")
     
     
-    return {}
+    return {""}
 
 def bing_search(state: State):
     return
