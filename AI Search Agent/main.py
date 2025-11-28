c3b8e82b0ef57cb8860d93fc90dd918ebcb7c9c6
IT21314742 <it21314742@my.sliit.lk>
2025-11-28 09:39:19 +0530

    Update
    Update

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -28,7 +28,7 @@ class state(TypedDict):
     final_answer: str | None
 
 class RedditURLAnalysis(BaseModel):
-    selected_urls: list[str] = Field(description=)
+    selected_urls: list[str] = Field(description= "")
 def google_search(state: State):
     return
 
