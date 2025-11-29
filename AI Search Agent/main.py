0c4dbcc0e2773e135f8263e89fcf34e2e8a974d8
IT21314742 <it21314742@my.sliit.lk>
2025-11-29 17:53:22 +0530

    Update Bing Configuration
    Update Bing Configuration

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -45,7 +45,7 @@ def google_search(state: State):
 
 def bing_search(state: State):
     user_question = state.get("user_question", "")
-    print()
+    print(f)
     return
 
 def reddit_search(state: State):
