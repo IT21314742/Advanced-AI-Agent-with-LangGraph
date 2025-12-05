fd42edcfebc28f2e11add9b9601fb98e815c278b
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-05 08:35:00 +0530

    Configure analyze reddit_posts function
    Configure analyze reddit_posts function

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -68,6 +68,6 @@ def analyze_reddit_posts(state: State):
     user_question = state.get("user_question","")
     reddit_results = state.get("reddit_results", "")
 
-    if not reddit_results
+    if not reddit_results:
 
 
