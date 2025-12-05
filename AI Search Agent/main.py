6d915fc988c5f3eaae70e0a353e673b35bb026bf
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-05 08:40:45 +0530

    Configure analyze reddit_posts function
    Configure analyze reddit_posts function

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -72,6 +72,6 @@ def analyze_reddit_posts(state: State):
         return {"selected_reddit_urls": []}
     
     structured_llm = llm.with_structutred_output(RedditURLAnalysis)
-    messages = get_reddit_url
+    messages = get_reddit_url_analysis
 
 
