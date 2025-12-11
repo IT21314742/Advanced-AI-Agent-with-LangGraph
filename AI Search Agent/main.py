a0b402bdc3474eaff3e3166bb896791eeb9ed2c8
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-11 11:17:02 +0530

    Configure Google Result Analysis Function
    Configure Google Result Analysis Function

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -123,6 +123,6 @@ def analyze_google_results(state: State):
 
 
     user_question = state.get("user_question","")
-    google_results = state.get("google_results",)
+    google_results = state.get("google_results", )
 
 
