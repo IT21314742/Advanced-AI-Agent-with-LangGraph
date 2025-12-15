df25202984e3b66775dbaec5b1bcb6ea4d9b8716
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-15 09:36:16 +0530

    Configure Google Result Analysis Function
    Configure Google Result Analysis Function

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -138,4 +138,4 @@ def analyze_bing_results(state: State):
     user_question = state.get("user_question", "")
     bing_results = state.get("bing_results", "")
 
-    messages = get_bing_
+    messages = get_bing_analysis
