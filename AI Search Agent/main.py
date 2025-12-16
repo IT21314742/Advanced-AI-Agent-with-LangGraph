1734d050d4c8671211fcd4bf342cd91997890ede
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-16 14:40:55 +0530

    Configure Reddit Result Analysis Function
    Configure Reddit Result Analysis Function

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -149,4 +149,4 @@ def analyze_reddit_results(state: State):
 
 
     user_question = state.get("user_question", "")
-    reddit_results = state.get("reddit_results")
+    reddit_results = state.get("reddit_results",)
