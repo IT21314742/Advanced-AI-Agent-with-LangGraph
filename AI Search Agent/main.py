6f9af63b5eca2759d3c604f5a093c5f1dd9c4fdc
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-18 15:29:38 +0530

    Configure Synthesis Analysis Function
    Configure Synthesis Analysis Function

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -163,5 +163,5 @@ def synthesize_analysis(state: State):
 
     user_question = state.get("user_question", "")
     google_analysis = state.get("google_Analysis", "")
-    bing_analysis = state.get("")  
+    bing_analysis = state.get("bing")  
     
