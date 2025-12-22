f2d7ebaa4eec134d6e7a1bf07f112e861dc45973
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-22 09:30:12 +0530

    Configure Graph states
    Configure Graph states

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -188,4 +188,5 @@ graph_builder.add_node("analyze_bing_results", analyze_bing_results)
 graph_builder.add_node("analyze_reddit_results", analyze_reddit_results)
 graph_builder.add_node("synthesize_analysis", synthesize_analysis)
 
+
                                                         
