e32ffc16b6fd286be9ec214ccd1b7c8a3241c3d6
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-22 09:39:40 +0530

    Configure Graph builder EDGE Configuration
    Configure Graph builder EDGE Configuration

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -191,5 +191,5 @@ graph_builder.add_node("synthesize_analysis", synthesize_analysis)
 
 graph_builder.add_edge(START, "google_search")
 graph_builder.add_edge(START, "bing_search")
-graph_builder.add_edge(START, )
+graph_builder.add_edge(START, "")
                                                         
