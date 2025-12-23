5dc318f9957996d1751ed86d5ecbf548a6f88fcf
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-23 09:38:00 +0530

    Configure Graph builder EDGE Configurations
    Configure Graph builder EDGE Configurations

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -200,4 +200,5 @@ graph_builder.add_edge("reddit_search", "analyze_reddit_posts")
 graph_builder.add_edge("analyze_reddit_posts", "retrieve_reddit_posts")
 
 
+
                                                         
