6ba35bfbcaf95f3e265f92a9e9897498ec987cad
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-29 10:15:53 +0530

    configuring edge nodes in graph
    configuring edge nodes in graph

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -228,6 +228,6 @@ def run_chatbot():
             break
 
         state = {
-            "messages": [{"role": "user", "content": user_}]
+            "messages": [{"role": "user", "content": user_input}]
         }
                                                         
