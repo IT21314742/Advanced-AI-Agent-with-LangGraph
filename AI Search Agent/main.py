6e8f69286949f2b8e0032b1b8c6d1e61ec5faf25
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-29 09:57:34 +0530

    configuring edge nodes in graph
    configuring edge nodes in graph

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -219,6 +219,6 @@ graph = graph_builder.compile()
 
 def run_chatbot():
     print("Multi-source Research Agent")
-    print("Type 'exit' to quit\")
+    print("Type 'exit' to quit\n")
 
                                                         
