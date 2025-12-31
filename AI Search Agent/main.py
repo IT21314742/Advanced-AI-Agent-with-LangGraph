6eaaab15c6c5e749e75cd1a460c441849e56c51a
Vihan Jayasinghe <it21314742@my.sliit.lk>
2025-12-31 16:21:14 +0530

    Configure Chatbot Programming
    Configure Chatbot Programming

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -233,6 +233,6 @@ def run_chatbot():
             "google_results": None,
             "bing_results": None,
             "reddit_results": None,
-            "selected_reddit_urls":
+            "selected_reddit_urls": 
         }
                                                         
