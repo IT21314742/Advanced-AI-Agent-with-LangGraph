ec5ffe0f642afc7e39a9a9cf08cbfd9570c10226
IT21314742 <it21314742@my.sliit.lk>
2026-01-03 18:30:14 +0530

    Update the source code
    Update the source code

--- AI Search Agent/main.py
+++ AI Search Agent/main.py
@@ -211,7 +211,7 @@ graph_builder.add_edge("analyze_reddit_results", "synthesize_analysis")
 graph_builder.add_edge("synthesize_analysis", END)
 
 graph = graph_builder.compile()
-                                                        
+                                                    
 
 
 
