ce1dc628bd44f255ef21e0f54ceac73ff7d34143
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-01-06 11:31:20 +0530

    Define System prompt for analyzing
    Define System prompt for analyzing

--- AI Search Agent/prompts.py
+++ AI Search Agent/prompts.py
@@ -22,5 +22,5 @@ Provide a concise analysis highlighting the most relevent findings."""
 
 
     @staticmethod
-    def google_analysis_user(user_question)
+    def google_analysis_user(user_question:)
 
