18767d4729eaf1e84d490f2744d9f7fbdd1c042a
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-01-31 13:35:21 +0530

    Update
    Update

--- AI Search Agent/snapshot_operations.py
+++ AI Search Agent/snapshot_operations.py
@@ -66,5 +66,5 @@ def download_snapshot(
 
           data = response.json()
           print(
-               f"🎉 Successfully download {len(data) if isinstance(data)}"
+               f"🎉 Successfully download {len(data) if isinstance(data, )}"
           )
