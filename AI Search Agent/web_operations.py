ebdcde13eeac1b7af1318fd63ee3e760ad6b94e8
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-02-02 12:51:03 +0530

    Config Snapshot Final Configurations : Downloads
    Config Snapshot Final Configurations : Downloads

--- AI Search Agent/web_operations.py
+++ AI Search Agent/web_operations.py
@@ -65,5 +65,6 @@ def _trigger_and_download_snapshot(trigger_url, params, data, operatrion_name="o
     
     snapshot_id = trigger_result.get("snapshot_id")
     if not snapshot_id:
+        return None
 
 
