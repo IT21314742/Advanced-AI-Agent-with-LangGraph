3c25f0bd9ab9315200f9658fe6ff90501bda5bb3
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-02-02 12:46:11 +0530

    Mapping Extracted Data
    Mapping Extracted Data

--- AI Search Agent/web_operations.py
+++ AI Search Agent/web_operations.py
@@ -55,6 +55,6 @@ def serp_search(query, engine="google"):
         "knwoledge": full_response.get("knowledge", {}),
         "organic": full_response.get("organic", []),
     }
-    
+    return extracted_data
 
 
