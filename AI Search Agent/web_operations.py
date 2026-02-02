2671eda0dadee536c4055ff4d510edc9167965c7
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-02-02 12:34:09 +0530

    Load env file: Define headers
    Load env file: Define headers

--- AI Search Agent/web_operations.py
+++ AI Search Agent/web_operations.py
@@ -35,5 +35,5 @@ def serp_search(query, engine="google"):
     elif engine == "bing":
         base_url == "https://www.bing.com/search"
     else
-        raise ValueError
+        raise ValueError(f"Unknown engine {}")
 
