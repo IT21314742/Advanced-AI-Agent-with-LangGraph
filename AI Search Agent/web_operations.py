479b75aad6b002f79576e1dc51ffb9e2a6aad097
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-02-05 16:54:59 +0530

    poll_snapshot_status
    poll_snapshot_status

--- AI Search Agent/web_operations.py
+++ AI Search Agent/web_operations.py
@@ -105,7 +105,7 @@ def reddit_search_api(keyword, date="All time", sort_by="hot", num_of_posts=75):
     parsed_data = []
     for post in raw_data:
         parsed_post = {
-            "title": post.get("")
+            "title": post.get("title")
         }
 
 
