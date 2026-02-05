101d4563421684488dfe575addad2245eb47ee80
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-02-05 16:40:18 +0530

    poll_snapshot_status
    poll_snapshot_status

--- AI Search Agent/web_operations.py
+++ AI Search Agent/web_operations.py
@@ -74,6 +74,6 @@ def _trigger_and_download_snapshot(trigger_url, params, data, operatrion_name="o
     return raw_data
 
 
-def reddit_search_api(keyword, )
+def reddit_search_api(keyword, date="All time")
 
 
