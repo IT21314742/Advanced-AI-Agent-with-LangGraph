fed2d9d46905eb62d19c5f62eb845d2f6d5f5452
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-02-06 10:12:11 +0530

    poll_snapshot_status: Web operations
    poll_snapshot_status: Web operations

--- AI Search Agent/web_operations.py
+++ AI Search Agent/web_operations.py
@@ -108,5 +108,5 @@ def reddit_search_api(keyword, date="All time", sort_by="hot", num_of_posts=75):
             "title": post.get("title"),
             "url": post.get("url")
         }
-
+        parsed_data
 
