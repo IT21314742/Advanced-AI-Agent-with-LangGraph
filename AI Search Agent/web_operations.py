16d6271c40ac5e5f02a047c42df5d001c8507098
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-02-06 10:25:04 +0530

    reddit_post_retrieval : Web operations
    reddit_post_retrieval : Web operations

--- AI Search Agent/web_operations.py
+++ AI Search Agent/web_operations.py
@@ -135,5 +135,5 @@ def reddit_post_retrieval(urls, days_back=10, load_all_replies=False, comment_li
     ]
 
 raw_data = _trigger_and_download_snapshot(
-    trigger_url, params, data, operatrion_name=
+    trigger_url, params, data, operatrion_name="reddit comments"
 )
